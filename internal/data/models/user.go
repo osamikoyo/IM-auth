1283c197c8e8bf32a17453f41eb644bc5e10b5c5
osamikoyo <v.pylayev@mail.ru>
2025-02-17 20:12:27 +0500

    proto add

--- internal/data/models/user.go
+++ internal/data/models/user.go
@@ -1,9 +1,28 @@
 package models
 
+import "github.com/osamikoyo/IM-auth/pkg/pb"
+
 type User struct{
-	ID uint `gorm:"primaryKey;autoIncrement"`
+	ID uint64 `gorm:"primaryKey;autoIncrement"`
 	Username string
 	Email string
 	Password string
 }
 
+func ToModels(u *pb.User) *User {
+	return &User{
+		ID: u.ID,
+		Username: u.Username,
+		Password: u.Password,
+		Email: u.Email,
+	}
+}
+
+func ToProtoBuf(u *User) *pb.User {
+	return &pb.User{
+		ID: u.ID,
+		Email: u.Email,
+		Password: u.Password,
+		Username: u.Username,
+	}
+}
