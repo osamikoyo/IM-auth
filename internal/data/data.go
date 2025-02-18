6927b1e29a38339ea0ab2e44730140d7eb07e03b
osamikoyo <v.pylayev@mail.ru>
2025-02-18 05:25:12 +0500

    data funcs

--- internal/data/data.go
+++ internal/data/data.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"github.com/osamikoyo/IM-auth/internal/config"
+	"github.com/osamikoyo/IM-auth/internal/data/models"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -21,4 +23,36 @@ func New(cfg *config.Config) (*Storage, error) {
 	}, nil
 }
 
+func (s *Storage) Register(user *models.User) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil{
+		return err
+	}
 
+	user.Password = string(hash)
+
+	return s.db.Create(user).Error
+}
+
+func generateToken(user *models.User) (string, error){
+	jwts, err := 
+}
+
+func (s *Storage) Login(email, password string) (string, error) {
+	var user models.User
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil{
+		return "", err
+	}
+
+	result := s.db.Where(&models.User{
+		Email: email,
+		Password: password,
+	}).Find(&user)
+	if result.Error != nil{
+		return "", err
+	}
+
+
+}
